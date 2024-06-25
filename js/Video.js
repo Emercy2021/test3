293893fe87fac9ab3040dfa423a1c072963753d4
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-06-25 10:55:48 +0300

    Update Video.js

--- js/Video.js
+++ js/Video.js
@@ -108,9 +108,10 @@ class Video {
         }
     }
 
+
     static _shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
+        for (let i = array.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random(array.length) * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
     }
